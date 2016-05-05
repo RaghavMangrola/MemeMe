b7eb4b566f9882173f3f26687a9522c33df06a6c
Raghav Mangrola <raghav.mangrola@gmail.com>
2016-05-05 08:23:40 -0700

    Basic Setup
    Can take an image and display into image view

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+  
+  let imagePickerVC = UIImagePickerController()
+  
+  @IBOutlet weak var memeImageView: UIImageView!
+  @IBOutlet weak var cameraButton: UIBarButtonItem!
+  
+  @IBAction func cameraButtonPressed(sender: AnyObject) {
+    presentImagePickerVC(sourceType: .Camera)
+  }
+  
+  @IBAction func albumButtonPressed(sender: AnyObject) {
+    presentImagePickerVC(sourceType: .PhotoLibrary)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    imagePickerVC.delegate = self
+    memeImageView.contentMode = .ScaleAspectFit
+    cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func presentImagePickerVC(sourceType sourceType: UIImagePickerControllerSourceType) {
+    imagePickerVC.sourceType = sourceType
+    presentViewController(imagePickerVC, animated: true, completion: nil)
   }
-
-
+  
+  // UIImagePickerControllerDelegate Functions
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+    
+    if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+      memeImageView.image = image
+    }
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
 }
 
