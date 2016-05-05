cd1f5874867f6eb5a06930ee3160d3f1a2d6d70b
Raghav Mangrola <raghav.mangrola@gmail.com>
2016-05-05 09:02:21 -0700

    setup textfields

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -11,9 +11,19 @@ import UIKit
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
   
   let imagePickerVC = UIImagePickerController()
+  let memeTextAttributes = [
+    NSStrokeColorAttributeName : UIColor.blackColor(),
+    NSForegroundColorAttributeName : UIColor.whiteColor(),
+    NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+    NSStrokeWidthAttributeName : 3.0
+  ]
   
   @IBOutlet weak var memeImageView: UIImageView!
   @IBOutlet weak var cameraButton: UIBarButtonItem!
+  @IBOutlet weak var topTextField: UITextField!
+  @IBOutlet weak var bottomTextField: UITextField!
+  @IBOutlet var textFields: [UITextField]!
+  
   
   @IBAction func cameraButtonPressed(sender: AnyObject) {
     presentImagePickerVC(sourceType: .Camera)
@@ -29,11 +39,20 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     imagePickerVC.delegate = self
     memeImageView.contentMode = .ScaleAspectFit
     cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+    
+    setupTextFields()
   }
   
   func presentImagePickerVC(sourceType sourceType: UIImagePickerControllerSourceType) {
     imagePickerVC.sourceType = sourceType
     presentViewController(imagePickerVC, animated: true, completion: nil)
+  }
+  
+  func setupTextFields() {
+    for textField in textFields {
+      textField.defaultTextAttributes = memeTextAttributes
+      textField.textAlignment = .Center
+    }
   }
   
   // UIImagePickerControllerDelegate Functions
